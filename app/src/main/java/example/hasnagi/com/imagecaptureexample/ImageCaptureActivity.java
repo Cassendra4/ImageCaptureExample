0a123a430244546348790dc5bc9a9fc727fefa49
codingfanatic <lclarkerichard@yahoo.com>
2020-04-27 16:45:20 -0400

    Updated workflow to request permissions The app now requests storage, camera, and location permissions onCreate before the workflow can start

--- app/src/main/java/example/hasnagi/com/imagecaptureexample/ImageCaptureActivity.java
+++ app/src/main/java/example/hasnagi/com/imagecaptureexample/ImageCaptureActivity.java
@@ -1,36 +1,72 @@
 package example.hasnagi.com.imagecaptureexample;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
-
-/**
- * @author Hasangi Thathsarani
- */
+import android.widget.Toast;
 
 public class ImageCaptureActivity extends AppCompatActivity {
 
     private ImageView imageView;
+    private String logtag = "TESTING";
+    private int STORAGE_PERMISSION_CODE = 1; //Identify specific requests for a permission
+    private int CAMERA_PERMISSION_CODE = 2;
+    private int LOCATION_PERMISSION_CODE = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_capture);
+
+        if (ContextCompat.checkSelfPermission(
+                ImageCaptureActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(ImageCaptureActivity.this, "You already granted storage permissions", Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            requestStoragePermission();
+        }
+
+        if (ContextCompat.checkSelfPermission(ImageCaptureActivity.this, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(ImageCaptureActivity.this, "You already granted Camera permissions", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            requestCameraPermission();
+        }
+
+        if (ContextCompat.checkSelfPermission(
+                ImageCaptureActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(ImageCaptureActivity.this, "Location resources already granted", Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            requestLocationPermission();
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -45,33 +81,130 @@ public class ImageCaptureActivity extends AppCompatActivity {
         });
     }
 
+    protected void onStart(Bundle savedInstanceState) {
+
+    }
+
+    private void requestStoragePermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed because of this and that")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(ImageCaptureActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .create().show();
+        }
+        else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+    }
+
+    private void requestCameraPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed to access the Camera")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(ImageCaptureActivity.this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    }).create().show();
+        }
+        else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        }
+    }
+
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission Needed")
+                    .setMessage("This permission is needed to confirm the location of the photos submitted")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(ImageCaptureActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    }).create().show();
+        }
+        else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == STORAGE_PERMISSION_CODE){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this,"Permission DENIEd", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     private void selectImage(Context context) {
+        //The CharSequence object will contain the options displayed in the Dialog
         final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
 
+        //Builder is a child of AlertDialog class
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setTitle("Choose your profile picture");
 
+        //Add the options from the CharSequence object and make them clickable
         builder.setItems(options, new DialogInterface.OnClickListener() {
 
+            //Override onClick method and configure what each option will do when clicked
             @Override
             public void onClick(DialogInterface dialog, int item) {
 
+                //Create Intents for taking a picture or accessing external storage
                 if (options[item].equals("Take Photo")) {
                     Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
                     startActivityForResult(takePicture, 0);
 
-                } else if (options[item].equals("Choose from Gallery")) {
+                }
+
+                else if (options[item].equals("Choose from Gallery")) {
                     Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                     startActivityForResult(pickPhoto, 1);//one can be replaced with any action code
+                    Log.d(logtag, "Gallery selected");
 
                 } else if (options[item].equals("Cancel")) {
                     dialog.dismiss();
+                    Log.d(logtag, "Cancel selected");
                 }
             }
         });
+        //Display the Dialog
         builder.show();
     }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -79,15 +212,16 @@ public class ImageCaptureActivity extends AppCompatActivity {
             switch (requestCode) {
                 case 0:
                     if (resultCode == RESULT_OK && data != null) {
-                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
-                        imageView.setImageBitmap(selectedImage);
+                        Bitmap takenImage = (Bitmap) data.getExtras().get("data");
+                        imageView.setImageBitmap(takenImage);
                     }
-
                     break;
+
                 case 1:
                     if (resultCode == RESULT_OK && data != null) {
                         Uri selectedImage = data.getData();
                         String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
                         if (selectedImage != null) {
                             Cursor cursor = getContentResolver().query(selectedImage,
                                     filePathColumn, null, null, null);
